86c9357aa57ce492d2c5099fc5bd817852718ae2
Artur <siesoft678@gmail.com>
2026-02-11 18:29:57 +0300

    init

---
+++ src/main/kotlin/com/foresko/zitadelextensions/ZitadelExtensionsApplication.kt
@@ -0,0 +1,11 @@
+package com.foresko.zitadelextensions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZitadelExtensionsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZitadelExtensionsApplication>(*args)
+}
---
+++ src/test/kotlin/com/foresko/zitadelextensions/ZitadelExtensionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.foresko.zitadelextensions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZitadelExtensionsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

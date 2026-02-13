92fda9210130df3bab259ab2ae6ed175da41bd3b
Artur <siesoft678@gmail.com>
2026-02-13 18:39:13 +0300

    wip

--- src/main/kotlin/com/foresko/zitadelextensions/client/ListEventsRequest.kt
+++ src/main/kotlin/com/foresko/zitadelextensions/client/ListEventsRequest.kt
@@ -1,5 +1,7 @@
 package com.foresko.zitadelextensions.client
 
+import java.time.OffsetDateTime
+
 data class ListEventsRequest(
     val sequence: Long? = null,
     val limit: Int? = null,
@@ -9,5 +11,6 @@ data class ListEventsRequest(
     val aggregateId: String? = null,
     val aggregateTypes: List<String>? = null,
     val resourceOwner: String? = null,
-    val creationDate: String? = null
+    val from: OffsetDateTime? = null,
+    val creationDate: OffsetDateTime? = null
 )
